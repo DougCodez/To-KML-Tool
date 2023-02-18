9dbadfaa8dc5bc51fde1466ada19632b3b07e242
DougCodez <98244802+DougCodez@users.noreply.github.com>
2023-02-18 10:02:45 -0500

    Create json to kml tool test.py

---
+++ python files/json to kml tool test/json to kml tool test.py
@@ -0,0 +1,24 @@
+import simplekml
+import json
+
+kml = simplekml.Kml()
+
+def jsonToKML(features, geometry, coordinates, properties, name):
+    print("Type Exit for either rawDirectory or jsonFileTarget if you want to stop the program!")
+    while (True):
+        rawDirectoryInput = input("Type in the directory of where your json files are stored: ")
+        jsonFileTarget = input("Type in the file you want to make a kml file out of: ")
+        if jsonFileTarget == "Exit" or rawDirectoryInput == "Exit":
+            break
+        fileName = rawDirectoryInput + "\\" + jsonFileTarget
+        with open(fileName + ".json", 'r') as file:
+            data = json.load(file)
+        for feature in data[features]:
+            location = feature[geometry][coordinates]
+            kml.newpoint(name=feature[properties][name], coords=[(location[1], location[0])])
+        kml.save(fileName + ".kml")
+        print("File " + jsonFileTarget + ".kml has been created and saved!")
+        print("File will show up, once you exit the program!")
+        
+#Take a look at the the json test file as its used for this example       
+jsonToKML('features', 'geometry', 'coordinates', 'properties', 'name')
