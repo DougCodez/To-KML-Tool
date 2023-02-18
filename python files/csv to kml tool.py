dd8aa7bb57b20845310548cbf7237e71387cc786
DougCodez <98244802+DougCodez@users.noreply.github.com>
2023-02-18 09:58:57 -0500

    Create csv to kml tool.py

---
+++ python files/csv to kml tool.py
@@ -0,0 +1,27 @@
+import csv
+import simplekml
+
+kml = simplekml.Kml()
+
+
+def csvToKML(yesName, yesDescription, name, desc, lon, lat):
+    print("Type Exit for either rawDirectory or csvFileTarget if you want to stop the program!")
+    while (True):
+        rawDirectoryInput = input("Type in the directory of where your csv files are stored: ")
+        csvFileTarget = input("Type in the file you want to make a kml file out of: ")
+        if csvFileTarget == "Exit" or rawDirectoryInput == "Exit":
+            break
+        fileName = rawDirectoryInput + "\\" + csvFileTarget
+        data = csv.reader(open(fileName + ".csv", 'r'))
+        for row in list(data)[1:]:
+            if yesName and yesDescription:
+                kml.newpoint(name=row[name], description=row[desc], coords=[(row[lon], row[lat])])
+            elif not yesName and not yesDescription:
+                kml.newpoint(coords=[(row[lon], row[lat])])
+            elif not yesName and yesDescription:
+                kml.newpoint(description=row[desc], coords=[(row[lon], row[lat])])
+            elif yesName and not yesDescription:
+                kml.newpoint(name=row[name], coords=[(row[lon], row[lat])])
+        kml.save(csvFileTarget + ".kml")
+        print("File " + csvFileTarget + ".kml has been created and saved!")
+        print("File will show up, once you exit the program!")
